186d812f11273b1d6adf85335ab1ddc3d165bf0c
Benjamin <r0720066@student.thomasmore.be>
2020-06-13 18:02:24 +0200

    added files php

--- dashboard-month.php
+++ dashboard-month.php
@@ -6,9 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>Thermi - dashboard maand</title>
 </head>
 
@@ -18,8 +16,7 @@
         <h1 class="text-l font-bold uppercase spacing pt-3">Overzicht</h1>
     </div>
 
-    <nav
-        class="z-20 px-3 grid grid-cols-4 gap-2 w-screen text-xxs text-center text-white fixed bottom-0 h-20 bg-black uppercase font-bold tracking-widest sm:px-20">
+    <nav class="z-20 px-3 grid grid-cols-4 gap-2 w-screen text-xxs text-center text-white fixed bottom-0 h-20 bg-black uppercase font-bold tracking-widest sm:px-20">
         <a class="pt-10 bg-db no-hover-white selected" href="dashboard-week.html">Overzicht</a>
         <a class="pt-10 bg-ct no-hover-white not-selected" href="caretakers.html">Mantelzorgers</a>
         <a class="pt-10 bg-c no-hover-white not-selected" href="contact.html">Contact</a>
